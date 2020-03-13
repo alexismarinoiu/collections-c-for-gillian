9c071585278619b3ae1b660b3ce7dc834c41b3fe
Sacha Ayoun <sachaayoun@gmail.com>
2020-03-13 18:39:32 +0000

    update generateKlee

--- generateKlee.sh
+++ generateKlee.sh
@@ -8,7 +8,7 @@ then
 
   for filename in "for-klee"/**/*.c; do
     echo "transforming $filename"
-    sed -i '' -e '1s/^/#include <klee\/klee.h> /' $filename
+    sed -i for-'' -e 's/#include <gillian-c\/gillian-c.h>/#include <klee\/klee.h>/g' $filename
     comby 'int :[[x]] = __builtin_annot_intval("symb_int", :[[x]]);' 'int :[x]; klee_make_symbolic(&:[x], sizeof(int), ":[x]");' -d $(dirname $filename) $(basename $filename) -i
     comby 'char :[[x]] = __builtin_annot_intval("symb_int", :[[x]]);' 'char :[x]; klee_make_symbolic(&:[x], sizeof(char), ":[x]");' -d $(dirname $filename) $(basename $filename) -i
     comby 'char :[[x]] = (char) __builtin_annot_intval("symb_int", :[[x]]);' 'char :[x]; klee_make_symbolic(&:[x], sizeof(char), ":[x]");' -d $(dirname $filename) $(basename $filename) -i
